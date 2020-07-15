75c7c69da290c541252bef85aa243f558ff008dc
Ygor <ygor.maniz@gmail.com>
2020-07-15 18:52:29 -0300

    Updating db config

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/todo', {
-  useMongoClient: true,
+module.exports = mongoose.connect('mongodb://localhost/todo',  {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
